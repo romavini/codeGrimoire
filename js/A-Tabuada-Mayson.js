3f7e1e5bbcc74dc3fbb586259eb23d15efdca189
Mayson <108405747+maycanin@users.noreply.github.com>
2022-10-07 08:35:53 -0300

    primeira função e correção de exercicio tabuada

---
+++ js/A-Tabuada-Mayson.js
@@ -0,0 +1,6 @@
+let valor = parseFloat(prompt("Exiba a tabuada de 1 ao 10 do valor recebido."));
+
+for (let contador = 1; contador <=10; contador++){
+  const multiplic = valor * contador;
+  console.log(`${valor} x ${contador} = ${multiplic}`);
+};
