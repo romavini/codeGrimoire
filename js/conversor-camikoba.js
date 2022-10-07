889a96bda722add655ebbd8f501fe75e24a9cd44
Camila Kobayashi <31277885+camikoba@users.noreply.github.com>
2022-10-07 11:35:51 -0300

    conversor de temperatura

---
+++ js/conversor-camikoba.js
@@ -0,0 +1,23 @@
+console.log("Conversão de Celsius e Fahrenheit \n1 - Celsius para Fahrenheit \n2 - Fahrenheit para Celsius");
+
+let conversao = parseInt(prompt("Insira a opção"));
+
+function conversaoCparaF(C) {
+  return 1.8 * C + 32;
+};
+
+function conversaoFparC(F) {
+  return (F - 32) / 1.8;
+};
+
+while (conversao != 1 && conversao != 2) {
+  conversao = parseInt(prompt("Insira a opção"));
+};
+
+if (conversao === 1) {
+  let C = parseFloat(prompt("Digite a temperatura em graus Celsius"));
+  console.log(conversaoCparaF(C), "graus Fahrenheit");
+} else if (conversao === 2) {
+  let F = parseFloat(prompt("Digite a temperatura em Fahrenheit"));
+  console.log(conversaoFparC(F), "graus Celsius");
+};
